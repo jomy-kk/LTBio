5bb794bea85b3f66aae85afc244724efd3804a1d
jomy-kk <joaomiguelsaraiva@tecnico.ulisboa.pt>
2022-04-25 18:57:20 +0100

    Add Bitalino source (not implemented)

---
+++ scr/biosignals/Bitalino.py
@@ -0,0 +1,18 @@
+from scr.biosignals.BiosignalSource import BiosignalSource
+
+class Bitalino(BiosignalSource):
+    def __init__(self):
+        super().__init__()
+
+    def __str__(self):
+        return "Bitalino"
+
+    @staticmethod
+    def _read(path, type):
+        '''Reads multiple TXT files on the directory 'path' and returns a Biosignal not associated to a Patient.'''
+        # TODO
+
+    @staticmethod
+    def _write(path):
+        '''Writes multiple TXT files on the directory 'path' so they can be opened in Opensignals.'''
+        # TODO
