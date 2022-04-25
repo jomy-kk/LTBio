fe4f65309cfab7c7d52852b320a7b30440f77a17
jomy-kk <joaomiguelsaraiva@tecnico.ulisboa.pt>
2022-04-25 17:12:11 +0100

    Add more Biosignal tests

--- tests/biosignals/Biosignal.py
+++ tests/biosignals/Biosignal.py
@@ -7,51 +7,73 @@ from scr.clinical.Epilepsy import Epilepsy
 from scr.clinical.Patient import Patient
 from scr.biosignals.ECG import ECG
 from scr.biosignals.HSM import HSM
-from scr.clinical.BodyLocation import BodyLocation
 
 
 class BiosignalTestCase(unittest.TestCase):
 
-    def create_biosignal_with_array(self):
-        ts1 = Timeseries([0, 1, 2], 128, Unit.V)
-        ts2 = Timeseries([3, 4, 5], 128, Unit.V)
-        ts3 = Timeseries([6, 7, 8], 128, Unit.V)
-        condition = Epilepsy()
-        patient = Patient(101, "João Miguel Areias Saraiva", 23, (condition,), tuple(), tuple())
-        ecg1 = ECG({BodyLocation.V1:ts1, BodyLocation.RA:ts2, BodyLocation.LA:ts3}, patient, HSM, BodyLocation.CHEST, "Test")
+    def setUp(self):
+        self.condition = Epilepsy()
+        self.patient = Patient(101, "João Miguel Areias Saraiva", 23, (self.condition,), tuple(), tuple())
+        self.ts1 = Timeseries([0.34, 2.12, 3.75], 128, Unit.V)
+        self.ts2 = Timeseries([1.34, 3.12, 4.75], 128, Unit.V)
+        self.ts3 = Timeseries([2.34, 4.12, 5.75], 128, Unit.V)
 
-        self.assertEqual(ecg1.type, ECG)
-        self.assertEqual(ecg1.n_channels, 3)
-        self.assertEqual(ecg1.channel_names[0], (BodyLocation.V1, BodyLocation.RA, BodyLocation.LA))
-        self.assertIn(1, ecg1[BodyLocation.V1])
-        self.assertIn(4, ecg1[BodyLocation.RA])
-        self.assertIn(7, ecg1[BodyLocation.LA])
-        self.assertEqual(ecg1.patient_code, 104)
-        self.assertEqual(ecg1.patient_conditions[0], condition)
+    def test_get_metadata(self):
+        ecg1 = ECG({"a": self.ts1}, HSM, self.patient, BodyLocation.CHEST, "Test")
+        self.assertEqual(ecg1.type, ECG.__name__)
+        self.assertEqual(ecg1.n_channels, 1)
+        self.assertEqual(ecg1.channel_names, ("a", ))
+        self.assertEqual(ecg1.patient_code, 101)
+        self.assertEqual(ecg1.patient_conditions[0], self.condition)
         self.assertEqual(ecg1.source, HSM)
         self.assertEqual(ecg1.acquisition_location, BodyLocation.CHEST)
         self.assertEqual(ecg1.name, "Test")
 
+    def test_create_biosignal_with_array(self):
+        ecg1 = ECG({"a":self.ts1, "b":self.ts2, "c":self.ts3})
+        self.assertEqual(ecg1.n_channels, 3)
+        self.assertEqual(ecg1.channel_names, ("a", "b", "c"))
+        self.assertTrue(isinstance(ecg1['a'], Timeseries) and ecg1['a'].n_samples == 3)
+        self.assertTrue(isinstance(ecg1['b'], Timeseries) and ecg1['b'].n_samples == 3)
+        self.assertTrue(isinstance(ecg1['c'], Timeseries) and ecg1['c'].n_samples == 3)
 
-    def set_name(self):
+    def test_create_biosignal_with_file(self):
+        ecg1 = ECG("resources/101_FA7775L1.edf", HSM) # a Video-EEG recording
+        self.assertEqual(ecg1.source, HSM)
+        self.assertEqual(ecg1.n_channels, 1)
+        self.assertEqual(ecg1.channel_names, ("ECG"))
+        self.assertTrue(ecg1[0] == 0.23487)
+
+    def test_create_biosignal_with_BodyLocation_on_channel_names(self):
+        ecg1 = ECG({BodyLocation.V1:self.ts1, }, )
+        self.assertEqual(ecg1.n_channels, 1)
+        self.assertEqual(ecg1.channel_names[0], BodyLocation.V1)
+
+    def test_set_name(self):
         ecg1 = ECG(dict(), name="Old Name")
         self.assertEqual(ecg1.name, "Old Name")
         ecg1.name = "New Name"
         self.assertEqual(ecg1.name, "New Name")
 
-
-    def get_unique_channel(self):
-        ts1 = Timeseries([0.34, 1.12, 2.75], 128, Unit.V)
-        ecg1 = ECG({BodyLocation.V1: ts1, })
+    def test_get_unique_channel(self):
+        ecg1 = ECG({BodyLocation.V1: self.ts1, })
+        ecg2 = ECG({"a": self.ts2, })
         self.assertEqual(ecg1.n_channels, 1)
+        self.assertEqual(ecg2.n_channels, 1)
 
-        y = ecg1[BodyLocation.V1]
-        self.assertRaises(Exception) # it is not supposed to index the unique channel of a biosignal
+        # it is not supposed to index the unique channel of a biosignal
+        with self.assertRaises(IndexError):
+            x = ecg1[BodyLocation.V1]
+        with self.assertRaises(IndexError):
+            x = ecg2["a"]
 
-        # it is supposed to access directly the values of the timeseries
-        self.assertEqual(ecg1[0], 0.34)
-        self.assertEqual(ecg1[1], 1.12)
-        self.assertEqual(ecg1[2], 2.75)
+        # instead, it is supposed to access directly the timeseries values
+        self.assertEqual(ecg1[0], self.ts1[0])
+        self.assertEqual(ecg1[1], self.ts1[1])
+        self.assertEqual(ecg1[2], self.ts1[2])
+        self.assertEqual(ecg2[0], self.ts2[0])
+        self.assertEqual(ecg2[1], self.ts2[1])
+        self.assertEqual(ecg2[2], self.ts2[2])
 
 
 
