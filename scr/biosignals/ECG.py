9e0421787e3ad08c15e553e6bd20088f2aadfd37
jomy-kk <joaomiguelsaraiva@tecnico.ulisboa.pt>
2022-04-25 16:56:27 +0100

    Remove redundant typing

--- scr/biosignals/ECG.py
+++ scr/biosignals/ECG.py
@@ -1,11 +1,5 @@
 from scr.biosignals.Biosignal import Biosignal
-from typing import Dict, Type, Union
-from scr.biosignals.Timeseries import Timeseries
-from scr.biosignals.BiosignalSource import BiosignalSource
-from scr.clinical.BodyLocation import BodyLocation
-from scr.clinical.Patient import Patient
-
 
 class ECG(Biosignal):
-    def __init__(self, timeseries: Dict[Union[str, BodyLocation], Timeseries], patient:Patient=None, source:Type[BiosignalSource]=None, acquisition_location:BodyLocation=None, name:str=None):
-        super(ECG, self).__init__(timeseries, patient, source, acquisition_location, name)
+    def __init__(self, timeseries, source=None, patient=None, acquisition_location=None, name=None):
+        super(ECG, self).__init__(timeseries, source, patient, acquisition_location, name)
