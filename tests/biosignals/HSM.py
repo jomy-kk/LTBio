f7dc587542815bc03a0f7125f5d1aba53a5b7777
jomy-kk <joaomiguelsaraiva@tecnico.ulisboa.pt>
2022-04-25 18:57:41 +0100

    Add HSM tests template (not implemented)

---
+++ tests/biosignals/HSM.py
@@ -0,0 +1,17 @@
+import unittest
+from scr.biosignals import (HSM, ECG)
+
+class HSMTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.HSM = HSM.HSM()
+        self.filepath = 'resources/101_FA7775L1.edf'
+
+
+    def test_read_ECG(self):
+        self.HSM._read(self.filepath, ECG.ECG)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
