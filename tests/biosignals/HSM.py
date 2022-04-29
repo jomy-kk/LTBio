a5f7a31e2857ee07e6636b821c23f53dff7b92df
jomy-kk <joaomiguelsaraiva@tecnico.ulisboa.pt>
2022-04-29 14:19:33 +0100

    Fix HSM._read test

--- tests/biosignals/HSM.py
+++ tests/biosignals/HSM.py
@@ -8,8 +8,9 @@ class HSMTestCase(unittest.TestCase):
 
     def setUp(self):
         self.HSM = HSM.HSM() # HSM needs to be instantiated only to test _read and _write methods, for they are protected.
-        self.testpath = 'resources/HSM_EDF_tests' # this is a test directory with EDF files in the HSM structure.
-        self.sampling_frequency = 512 # they have ECG signals sampled at 512 Hz,
+        self.testpath = 'resources/HSM_EDF_tests' # This is a test directory with EDF files in the HSM structure,
+        self.channel1, self.channel2 = "xx", "yy" # containing ECG channels with these names,
+        self.sampling_frequency = 512 # sampled at 512 Hz,
         self.n_samples = 1000 # with a total of this amount of samples,
         self.units = Unit.V # in the Volt unit.
         self.first_sample, self.last_sample = 420, 530  # The first and last samples are these values,
@@ -17,14 +18,29 @@ class HSMTestCase(unittest.TestCase):
 
     def test_read_ECG(self):
         x = self.HSM._read(self.testpath, ECG.ECG)
-        self.assertTrue(isinstance(x, Timeseries)) # _read should return a Timeseries
+        # _read should return a dictionary with 2 Timeseries, each corresponding to one ECG channel.
+        self.assertTrue(isinstance(x, dict))
+        self.assertEquals(len(x), 2)
+        self.assertTrue(isinstance(x.keys()[0], str))
+        self.assertTrue(isinstance(x.keys()[1], str))
+        self.assertEquals(x.keys()[0], self.channel1)
+        self.assertEquals(x.keys()[1], self.channel2)
+        self.assertTrue(isinstance(x[self.channel1], Timeseries))
+        self.assertTrue(isinstance(x[self.channel2], Timeseries))
         # And all these properties should match:
-        self.assertEquals(x.sampling_frequency, self.sampling_frequency)
-        self.assertEquals(x.n_samples, self.n_samples)
-        self.assertEquals(x.units, self.units)
+        self.assertEquals(x[self.channel1].sampling_frequency, self.sampling_frequency)
+        self.assertEquals(x[self.channel1].n_samples, self.n_samples)
+        self.assertEquals(x[self.channel1].units, self.units)
+        self.assertEquals(x[self.channel2].sampling_frequency, self.sampling_frequency)
+        self.assertEquals(x[self.channel2].n_samples, self.n_samples)
+        self.assertEquals(x[self.channel2].units, self.units)
         # Also, checking the first and last samples
-        self.assertEquals(x[self.first_timestamp], self.first_sample)
-        self.assertEquals(x[self.last_timestamp], self.last_sample)
+        self.assertEquals((x[self.channel1])[self.first_timestamp], self.first_sample)
+        self.assertEquals((x[self.channel1])[self.last_timestamp], self.last_sample)
+
+    def test_instantiate_ECG(self):
+        #TODO
+        pass
 
 
 
