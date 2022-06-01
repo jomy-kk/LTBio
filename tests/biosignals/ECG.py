8605075000dbb962debb98291ac670599f018e00
jomy-kk <joaomiguelsaraiva@tecnico.ulisboa.pt>
2022-06-01 14:35:11 +0100

    Fix unittests of ECG.plot_summary
    Working with MITDB patient 104 as example

--- tests/biosignals/ECG.py
+++ tests/biosignals/ECG.py
@@ -1,22 +1,22 @@
 import unittest
 
 from src.biosignals.ECG import ECG
-from src.biosignals.HSM import HSM
+from src.biosignals.MITDB import MITDB
 from src.processing.FrequencyDomainFilter import FrequencyDomainFilter, FrequencyResponse, BandType
 
 class ECGTestCase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.ecg = ECG('../test', HSM)
+        cls.ecg = ECG('resources/MITDB_DAT_tests', MITDB)
 
     def test_plotting_summary_biosppy_filtering_default(cls):
-        cls.ecg['2019-02-26 03:23:04':'2019-02-26 03:23:14']['POL Ecg'].plot_summary(show=True)
+        cls.ecg['2000-01-01 00:00:15':'2000-01-01 00:00:45']['V2'].plot_summary(show=True)
 
     def test_plotting_summary_own_filtering(cls):
         cls.ecg.filter(FrequencyDomainFilter(FrequencyResponse.FIR, BandType.LOWPASS, cutoff=20, order=4))
-        cls.ecg['2019-02-26 03:23:04':'2019-02-26 03:23:14']['POL Ecg'].plot_summary(show=True)
-        cls.ecg['2019-02-26 03:23:04':'2019-02-26 03:23:14']['POL Ecg'].undo_filters()
+        cls.ecg['2000-01-01 00:00:15':'2000-01-01 00:00:45']['V2'].plot_summary(show=True)
+        cls.ecg.undo_filters()
 
 if __name__ == '__main__':
     unittest.main()
