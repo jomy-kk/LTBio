b1a9f90db40fc7b6355cd7f72445fa36579b1fdf
jomy-kk <joaomiguelsaraiva@tecnico.ulisboa.pt>
2022-06-01 16:48:20 +0100

    Segmenter tests passing

--- tests/processing/Segmenter.py
+++ tests/processing/Segmenter.py
@@ -1,7 +1,7 @@
 import unittest
 from datetime import datetime, timedelta
 
-from src.biosignals.HSM import HSM
+from src.biosignals.MITDB import MITDB
 from src.biosignals.Timeseries import Timeseries
 from src.biosignals.ECG import ECG
 from src.processing.Segmenter import Segmeter
@@ -10,44 +10,62 @@ class SegmenterTestCase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
-        cls.testpath = 'resources/MITDB_DAT_tests/'  # This is a test directory with EDF files in the HSM structure,
-     
-        cls.samplesx1, cls.samplesx2, cls.samplesy1, cls.samplesy2 = [0.00023582690935384015,
-                                                                          0.00023582690935384015,
-                                                                          0.00023582690935384015], \
-                                                                         [0.0001709850882900646, 0.0001709850882900646,
-                                                                          0.0001709850882900646], \
-                                                                         [0.00023582690935384015,
-                                                                          0.00023582690935384015,
-                                                                          0.00023582690935384015], \
-                                                                         [0.0001709850882900646, 0.0001709850882900646,
-                                                                          0.0001709850882900646]
-        cls.initial1, cls.initial2 = datetime(2019, 2, 28, 8, 7, 16), datetime(2019, 2, 28, 10, 7,
-                                                                                 31)  # 1/1/2022 4PM and 3/1/2022 9AM
+        cls.testpath = 'resources/MITDB_DAT_tests/'  # This is a test directory with DAT files in the MIT-DB structure,
+
+        cls.samplesy = [0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.215, 0.235, 0.24, 0.24, 0.245,
+                                        0.265]
+        cls.samplesx = [-0.15, -0.15, -0.15, -0.15, -0.15, -0.15, -0.15, -0.15, -0.145, -0.135,
+                                                 -0.11, -0.08, -0.04, 0.0, 0.125]
+
+        cls.initial = datetime(2000, 1, 1, 0, 0, 0)  # 1/1/2000 0 AM
         cls.sf = 360
-        cls.segmentx1, cls.segmentx2 = Timeseries.Segment(cls.samplesx1, cls.initial1, cls.sf), \
-                                         Timeseries.Segment(cls.samplesx2, cls.initial2, cls.sf)
-        cls.segmenty1, cls.segmenty2 = Timeseries.Segment(cls.samplesy1, cls.initial1, cls.sf), \
-                                         Timeseries.Segment(cls.samplesy2, cls.initial2, cls.sf)
+        cls.segmentx, cls.segmenty = Timeseries.Segment(cls.samplesx, cls.initial, cls.sf), \
+                                       Timeseries.Segment(cls.samplesy, cls.initial, cls.sf)
 
-        cls.n_samplesx = 12000
-        cls.n_samplesy = 12000
+        cls.n_samplesx_trimmed = 649998
+        cls.n_samplesy_trimmed = 649998
 
-        cls.channelx, cls.channely = "POL Ecg", "POL  ECG-"
-        cls.tsx = Timeseries([cls.segmentx1, cls.segmentx2], True, cls.sf)
-        cls.tsy = Timeseries([cls.segmenty1, cls.segmenty2], True, cls.sf)
-        
+        cls.channelx, cls.channely = "V5", "V2"
+
+
     def test_segment_without_overlap(self):
-        ecg = ECG(self.testpath, HSM)
-        segmenter = Segmeter(timedelta(seconds=2))
+        ecg = ECG(self.testpath, MITDB)
+        segmenter = Segmeter(timedelta(milliseconds=10))
 
         ecg_segmented = segmenter.segment(ecg)
         x,y = ecg_segmented._Biosignal__timeseries[self.channelx], ecg_segmented._Biosignal__timeseries[self.channely]
 
-        self.assertEquals(len(x), self.n_samplesx)
-        self.assertEquals(len(y), self.n_samplesy)
-        self.assertEquals(x.segments[0].samples.tolist(), self.samplesx1[0:2])
-        self.assertEquals(x.segments[1].samples.tolist(), self.samplesx1[2:4])
+        self.assertEqual(len(x), 649998)
+        self.assertEqual(len(y), 649998)
+
+        segment_length = int(0.01*self.sf)  # each segment should have 3 samples
+        for segment in x.segments:
+            self.assertEqual(len(segment), segment_length)
+
+        for i, segmentx, segmenty in zip(range(0, len(self.samplesx), segment_length), x, y):
+            self.assertEqual(segmentx.samples.tolist(), self.samplesx[i:i+segment_length])
+            self.assertEqual(segmenty.samples.tolist(), self.samplesy[i:i+segment_length])
+
+
+    def test_segment_with_overlap(self):
+        ecg = ECG(self.testpath, MITDB)
+        segmenter = Segmeter(timedelta(milliseconds=10), timedelta(milliseconds=3))
+
+        ecg_segmented = segmenter.segment(ecg)
+        x,y = ecg_segmented._Biosignal__timeseries[self.channelx], ecg_segmented._Biosignal__timeseries[self.channely]
+
+        self.assertEqual(len(x), 1949994)
+        self.assertEqual(len(y), 1949994)
+
+        segment_length = int(0.01*self.sf)  # each segment should have 3 samples
+        for segment in x.segments:
+            self.assertEqual(len(segment), segment_length)
+
+        n_samples_overlap = int(0.003*self.sf)  # 1 sample of overlap
+        for i, segmentx, segmenty in zip(range(0, len(self.samplesx), segment_length-n_samples_overlap), x, y):
+            self.assertEqual(segmentx.samples.tolist(), self.samplesx[i:i+segment_length])
+            self.assertEqual(segmenty.samples.tolist(), self.samplesy[i:i+segment_length])
+
 
 if __name__ == '__main__':
     unittest.main()
