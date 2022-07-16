8e331a4c35eab8bf4d533a62ce88cde2ede70e6b
João Saraiva <joaomiguelsaraiva@tecnico.ulisboa.pt>
2022-07-16 17:25:15 +0100

    Add 'r_timepoints', 'heartbeats', 'hr', and 'nni' methods to ECG

--- src/biosignals/ECG.py
+++ src/biosignals/ECG.py
@@ -1,10 +1,16 @@
+from datetime import timedelta
+from typing import Dict
+
+import numpy as np
 from biosppy.plotting import plot_ecg
 from biosppy.signals.tools import get_heart_rate
 from biosppy.signals.ecg import hamilton_segmenter, correct_rpeaks, extract_heartbeats, ecg as biosppyECG
 from numpy import linspace
 
+from biosignals.Timeseries import Timeseries, OverlappingTimeseries
 from src.biosignals.Biosignal import Biosignal
-from src.biosignals.Unit import Volt, Multiplier
+from src.biosignals.Unit import Volt, Multiplier, BeatsPerMinute
+
 
 class ECG(Biosignal):
 
@@ -43,3 +49,164 @@ class ECG(Biosignal):
                               path=save_to,
                               show=show
                             )
+
+    def plot_rpeaks(self, show:bool=True, save_to:str=None):
+        pass  # TODO
+
+    def __r_indices(self, channel:Timeseries, algorithm_method=hamilton_segmenter) -> np.array:
+        """
+        Returns the indices of the R peaks, listed by Segment.
+        E.g. [ [30, 63, 135, ], [23, 49, 91], ... ], where [30, 63, 135, ] are the indices of the 1st Segment.
+        """
+
+        if hasattr(channel.segments[0], 'r_indices'):  # If previously computed, they were stored
+            return [segment._r_indices for segment in channel.segments]
+
+        else:
+            res = []
+            for segment in channel:
+                indices = algorithm_method(segment.samples, channel.sampling_frequency)['rpeaks']  # Compute indices
+                from biosppy.signals.ecg import correct_rpeaks
+                corrected_indices = correct_rpeaks(segment.samples, indices, channel.sampling_frequency)['rpeaks']  # Correct indices
+                res.append(corrected_indices)
+            return res
+
+    def r_timepoints(self, algorithm='hamilton') -> np.array:
+        """
+        Finds the timepoints of the R peaks.
+
+        @param algoritm (optional): The algorithm used to compute the R peaks. Default: Hamilton segmenter.
+
+        @returns: The ordered sequence of timepoints of the R peaks.
+        @rtype: np.array
+
+        Note: Index one channel first.
+        """
+
+        # Get segmenter function
+        if algorithm is 'ssf':
+            from biosppy.signals.ecg import ssf_segmenter
+            segmenter = ssf_segmenter
+        elif algorithm is 'christov':
+            from biosppy.signals.ecg import christov_segmenter
+            segmenter = christov_segmenter
+        elif algorithm is 'engzee':
+            from biosppy.signals.ecg import engzee_segmenter
+            segmenter = engzee_segmenter
+        elif algorithm is 'gamboa':
+            from biosppy.signals.ecg import gamboa_segmenter
+            segmenter = gamboa_segmenter
+        elif algorithm is 'hamilton':
+            segmenter = hamilton_segmenter
+        elif algorithm is 'asi':
+            from biosppy.signals.ecg import ASI_segmenter
+            segmenter = ASI_segmenter
+        else:
+            raise ValueError(
+                "Give an 'algorithm' from the following: 'ssf', 'christov', 'engzee', 'gamboa', 'hamilton', or 'asi'.")
+
+        channel = tuple(self._Biosignal__timeseries.values())[0]
+        r_indices = self.__r_indices(channel, segmenter)
+        all_timepoints = []
+        for x in r_indices:
+            timepoints = np.divide(x, channel.sampling_frequency)  # Transform to timepoints
+            all_timepoints += [timedelta(seconds=tp) for tp in timepoints]  # Append them all
+
+        return np.array(all_timepoints)
+
+    def heartbeats(self, before=0.2, after=0.4):
+        """
+        Segment the signal by heartbeats.
+        Works like a Segmenter, except output Timeseries is not necessarily equally segmented.
+
+        Parameters
+        ----------
+        before : float, optional
+            Window size to include before the R peak (seconds).
+        after : int, optional
+            Window size to include after the R peak (seconds).
+
+        Returns
+        -------
+        heartbeats : ECG
+            Biosignal segmented where each Segment is a heartbeat.
+
+        """
+
+        from biosppy.signals.ecg import extract_heartbeats
+
+        all_heartbeat_channels = {}
+        for channel_name in self.channel_names:
+            channel = self._Biosignal__timeseries[channel_name]
+            all_heartbeats = []
+            r_indices = self.__r_indices(channel)
+            for segment, indices in zip(channel, r_indices):
+                heartbeats = extract_heartbeats(segment.samples, indices, channel.sampling_frequency, before, after)['templates']
+                time_offset = segment.initial_datetime - timedelta(seconds=before)
+                for hb, r_index in zip(heartbeats, indices):
+                    all_heartbeats.append(Timeseries.Segment(hb, timedelta(seconds=r_index/channel.sampling_frequency)+time_offset, channel.sampling_frequency))
+
+            all_heartbeat_channels[channel_name] = OverlappingTimeseries(all_heartbeats, True, channel.sampling_frequency, channel.units, 'Heartbeats of ' + channel.name, equally_segmented=False)
+
+        return ECG(all_heartbeat_channels, self.source, self._Biosignal__patient, self.acquisition_location, 'Heartbeats of ' + self.name)
+
+    def hr(self, smooth_length: float = None):
+        """
+        Transform ECG signal to instantaneous heart rate.
+
+         Parameters
+        ----------
+        smooth_length : float, optional
+            Length of smoothing window. If not given, no smoothing is performed on the instantaneous heart rate.
+
+        Returns
+        -------
+        hr : HR
+            Pseudo-Biosignal where samples are the instantaneous heart rate at each timepoint.
+        """
+
+        from biosppy.signals.tools import get_heart_rate
+
+        all_hr_channels = {}
+        for channel_name in self.channel_names:
+            channel = self._Biosignal__timeseries[channel_name]
+            all_hr = []
+            for segment in channel:
+                indices = np.array([int((timepoint - segment.initial_datetime).total_seconds() * self.sampling_frequency) for timepoint in self.r_timepoints])
+                hr = get_heart_rate(indices, channel.sampling_frequency, smooth = (smooth_length is not None), size=smooth_length)['heart_rate']
+                all_hr.append(Timeseries.Segment(hr, segment.initial_datetime, channel.sampling_frequency))
+
+            all_hr_channels[channel_name] = Timeseries(all_hr, True, channel.sampling_frequency, BeatsPerMinute(), 'Heart Rate of ' + channel.name, equally_segmented=False)
+
+        from biosignals.HR import HR
+        return HR(all_hr_channels, self.source, self._Biosignal__patient, self.acquisition_location, 'Heart Rate of ' + self.name, original_signal=self)
+
+    def nni(self):
+        """
+        Transform ECG signal to R-R peak interval signal.
+
+        Parameters
+        ----------
+        smooth_length : float, optional
+            Length of smoothing window. If not given, no smoothing is performed on the instantaneous heart rate.
+
+        Returns
+        -------
+        hr : HR
+            Pseudo-Biosignal where samples are the instantaneous heart rate at each timepoint.
+        """
+
+        all_nni_channels = {}
+        for channel_name in self.channel_names:
+            channel = self._Biosignal__timeseries[channel_name]
+            all_nii = []
+            for segment in channel:
+                indices = np.array([int((timepoint - segment.initial_datetime).total_seconds() * self.sampling_frequency) for timepoint in self.r_timepoints])
+                nni = np.diff(indices)
+                all_nii.append(Timeseries.Segment(nni, segment.initial_datetime, channel.sampling_frequency))
+
+            all_nni_channels[channel_name] = Timeseries(all_nii, True, channel.sampling_frequency, None, 'NNI of ' + channel.name, equally_segmented=channel.is_equally_segmented)
+
+        return NNI(all_nni_channels, self.source, self._Biosignal__patient, self.acquisition_location, 'NNI of ' + self.name, original_signal=self)
+
+
--- tests/biosignals/ECG.py
+++ tests/biosignals/ECG.py
@@ -1,4 +1,7 @@
 import unittest
+from datetime import timedelta
+
+import numpy as np
 
 from src.biosignals.ECG import ECG
 from src.biosignals.MITDB import MITDB
@@ -10,13 +13,23 @@ class ECGTestCase(unittest.TestCase):
     def setUpClass(cls):
         cls.ecg = ECG('resources/MITDB_DAT_tests', MITDB)
 
-    def test_plotting_summary_biosppy_filtering_default(cls):
-        cls.ecg['2000-01-01 00:00:15':'2000-01-01 00:00:45']['V2'].plot_summary(show=True)
+    def test_plotting_summary_biosppy_filtering_default(self):
+        self.ecg['2000-01-01 00:00:15':'2000-01-01 00:00:45']['V2'].plot_summary(show=True)
 
-    def test_plotting_summary_own_filtering(cls):
-        cls.ecg.filter(FrequencyDomainFilter(FrequencyResponse.FIR, BandType.LOWPASS, cutoff=20, order=4))
-        cls.ecg['2000-01-01 00:00:15':'2000-01-01 00:00:45']['V2'].plot_summary(show=True)
-        cls.ecg.undo_filters()
+    def test_plotting_summary_own_filtering(self):
+        self.ecg.filter(FrequencyDomainFilter(FrequencyResponse.FIR, BandType.LOWPASS, cutoff=20, order=4))
+        self.ecg['2000-01-01 00:00:15':'2000-01-01 00:00:45']['V2'].plot_summary(show=True)
+        self.ecg.undo_filters()
+
+    def test_r_peaks(self):
+        rpeaks = self.ecg['V5'].r_timepoints()
+        first_10_true_rpeaks = np.array([timedelta(microseconds=872222), timedelta(seconds=1, microseconds=702778), timedelta(seconds=2, microseconds=497222), timedelta(seconds=3, microseconds=294444), timedelta(seconds=4, microseconds=102778), timedelta(seconds=4, microseconds=855556), timedelta(seconds=5, microseconds=722222), timedelta(seconds=6, microseconds=552778), timedelta(seconds=7, microseconds=358333), timedelta(seconds=8, microseconds=166667)])
+        self.assertTrue(np.all(rpeaks[:10] == first_10_true_rpeaks))
+
+    def test_heartbeats(self):
+        heartbeats = self.ecg.heartbeats()
+        print(heartbeats)
+        heartbeats['2000-01-01 00:00:15':'2000-01-01 00:00:45'].plot()
 
 if __name__ == '__main__':
     unittest.main()
