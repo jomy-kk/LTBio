5f3930e62eb218f215ce4c35af242e63eb56da76
João Saraiva <joaomiguelsaraiva@tecnico.ulisboa.pt>
2022-09-11 14:55:35 +0100

    Add plot_summary headers to Biosignal subclasses

--- src/ltbio/biosignals/modalities/HR.py
+++ src/ltbio/biosignals/modalities/HR.py
@@ -20,6 +20,9 @@ from ltbio.biosignals.modalities.PPG import PPG
 
 
 class HR(Biosignal):
+    def plot_summary(self, show: bool = True, save_to: str = None):
+        pass
+
     def __init__(self, timeseries, source=None, patient=None, acquisition_location=None, name=None, original_signal:ECG|PPG=None):
         super(HR, self).__init__(timeseries, source, patient, acquisition_location, name)
         self.__original_signal = original_signal
