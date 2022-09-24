5fd2af31a2d4c9fb08e5d72220652253649ddde2
João Saraiva <joaomiguelsaraiva@tecnico.ulisboa.pt>
2022-09-24 21:17:02 +0100

    Add ValueToValueDataset and EventDetectionDataset

---
+++ src/ltbio/ml/datasets/EventDetectionDataset.py
@@ -0,0 +1,102 @@
+# -- encoding: utf-8 --
+
+# ===================================
+
+# IT - LongTermBiosignals
+
+# Package: ml
+# Module: EventDetectionDataset
+# Description: 
+
+# Contributors: João Saraiva
+# Created: 24/07/2022
+# Last Updated: 05/08/2022
+
+# ===================================
+from typing import Collection, overload, Iterable
+
+import numpy as np
+import pandas as pd
+from numpy import array
+from pandas import DataFrame
+
+from ltbio.biosignals.modalities.Biosignal import Biosignal
+from ltbio.biosignals.timeseries.Timeseries import Timeseries
+from ltbio.ml.datasets.BiosignalDataset import BiosignalDataset
+
+
+class EventDetectionDataset(BiosignalDataset):
+
+    @overload
+    def __init__(self, object: Iterable[Biosignal] | Biosignal, event_names: Iterable[str] | str, name: str = None): ...
+
+    def __init__(self, object, event_names, name: str = None):
+        super().__init__(name)
+
+        # Check object types
+        if isinstance(object, Biosignal):
+            object = (object, )
+        elif not (isinstance(object, Iterable) and all(isinstance(x, Biosignal) for x in object)):
+            raise TypeError("Parameter 'object' has to be a Biosignal or an iterable of Biosignals.")
+
+        # Assert not empty
+        if len(object) == 0:
+            raise AssertionError("Given object cannot be an empty collection.")
+
+        # Check if Event exists
+        if isinstance(event_names, str):
+            event_names = (event_names, )
+        if not (isinstance(event_names, Iterable) and all(isinstance(x, str) for x in event_names)):
+            raise TypeError("Parameter 'event_name' has to be a string or an iterable of strings.")
+        for biosignal in object:
+            for e in event_names:
+                if e not in biosignal:
+                    raise ValueError(f"No '{e}' Event is associated with Biosignal '{biosignal.name}'.")
+
+        # Check channel names
+        if len(object) > 1:
+            self._BiosignalDataset__object_timeseries_names = object[0].channel_names
+            for biosignal in object:
+                if biosignal.channel_names != self._BiosignalDataset__object_timeseries_names:
+                    raise AssertionError("The multiple Biosignals given must have the same channel names.")
+
+        # Make objects
+        positive_objects, negative_objects = {}, {}
+
+        for biosignal in object:
+            if len(positive_objects) == 0:  # initialization
+                for channel_name in biosignal.channel_names:
+                    positive_objects[channel_name], negative_objects[channel_name] = [], []  # with empty lists
+
+            for e in event_names:
+                positive = biosignal[e]  # domain U {event interval}
+                negative = biosignal[:'-'+e:]  # domain \ {event interval}
+                for channel_name, channel in positive:
+                    positive_objects[channel_name] += [channel.samples if channel.is_contiguous else np.concatenate(channel.samples, axis=0), ]
+                for channel_name, channel in negative:
+                    negative_objects[channel_name] += [channel.samples if channel.is_contiguous else np.concatenate(channel.samples, axis=0), ]
+
+        # Flatten objects and targets (of multiple biosignals and events)
+        for key in positive_objects.keys():
+            positive_objects[key] = np.concatenate(positive_objects[key], axis=0)
+            negative_objects[key] = np.concatenate(negative_objects[key], axis=0)
+
+        self._BiosignalDataset__objects = []
+        self._BiosignalDataset__targets = []
+
+        # Group by example
+        positive_objects = DataFrame(positive_objects)
+        negative_objects = DataFrame(negative_objects)
+        for i in range(len(positive_objects)):
+            self._BiosignalDataset__objects.append(array(positive_objects.iloc[i]))
+            self._BiosignalDataset__targets.append(1)  # Make target
+        for i in range(len(negative_objects)):
+            self._BiosignalDataset__objects.append(array(negative_objects.iloc[i]))
+            self._BiosignalDataset__targets.append(0)  # Make target
+
+        pass
+        # Shuffling is responsability of the user
+
+
+
+
---
+++ src/ltbio/ml/datasets/ValueToValueDataset.py
@@ -0,0 +1,105 @@
+# -- encoding: utf-8 --
+
+# ===================================
+
+# IT - LongTermBiosignals
+
+# Package: ml
+# Module: ValueToValueDataset
+# Description: 
+
+# Contributors: João Saraiva
+# Created: 24/07/2022
+# Last Updated: 05/08/2022
+
+# ===================================
+from typing import Collection, overload
+
+import numpy as np
+
+from ltbio.biosignals.modalities.Biosignal import Biosignal
+from ltbio.biosignals.timeseries.Timeseries import Timeseries
+from ltbio.ml.datasets.BiosignalDataset import BiosignalDataset
+
+
+class ValueToValueDataset(BiosignalDataset):
+
+    @overload
+    def __init__(self, object: Collection[Biosignal], target: Collection[Biosignal], name: str = None): ...
+    @overload
+    def __init__(self, object: Collection[Timeseries], target: Collection[Timeseries], name: str = None): ...
+
+    def __init__(self, object, target, name: str = None):
+        super().__init__(name)
+
+        # Check object types
+        if isinstance(object, Collection) and all(isinstance(x, Biosignal) for x in object):
+            self._BiosignalDataset__biosignals['object'] = object
+            res = []
+            self._BiosignalDataset__object_timeseries_names = []
+            for biosignal in object:
+                for channel_name, channel in biosignal:
+                    res.append(channel)
+                    self._BiosignalDataset__object_timeseries_names.append(channel_name)  # Save the order of the channels, by their names
+            object = res
+
+        elif isinstance(object, Collection) and all(isinstance(x, Timeseries) for x in object):
+            self._BiosignalDataset__object_timeseries_names = tuple([timeseries.name for timeseries in object])  # Save the order of the Timeseries, by their names
+        else:
+            raise ValueError("Parameter 'object' needs to be a collection of Biosignals or TImeseries.")
+
+        # Check duplicate object names:
+        self._BiosignalDataset__object_timeseries_names = tuple(self._BiosignalDataset__object_timeseries_names)
+        if len(self._BiosignalDataset__object_timeseries_names) != len(set(self._BiosignalDataset__object_timeseries_names)):
+            raise AssertionError("Not all object Timeseries given have distinct names. Give a unique name for each Timeseries.")
+
+        # Check target types
+        if isinstance(target, Collection) and all(isinstance(x, Biosignal) for x in target):
+            self._BiosignalDataset__biosignals['target'] = target
+            res = []
+            self._BiosignalDataset__target_timeseries_names = []
+            for biosignal in target:
+                for channel_name, channel in biosignal:
+                    res.append(channel)
+                    self._BiosignalDataset__target_timeseries_names.append(channel_name)  # Save the order of the channels, by their names
+            target = res
+
+        elif isinstance(target, Collection) and all(isinstance(x, Timeseries) for x in target):
+            self._BiosignalDataset__target_timeseries_names = tuple([timeseries.name for timeseries in target])  # Save the order of the Timeseries, by their names
+        else:
+            raise ValueError("Parameter 'target' needs to be a collection of Biosignals or Timeseries.")
+
+        # Check duplicate target names:
+        self._BiosignalDataset__target_timeseries_names = tuple(self._BiosignalDataset__target_timeseries_names)
+        if len(self._BiosignalDataset__target_timeseries_names) != len(set(self._BiosignalDataset__target_timeseries_names)):
+            raise AssertionError("Not all target Timeseries given have distinct names. Give a unique name for each Timeseries.")
+
+        # Assert not empty
+        if len(object) == 0:
+            raise AssertionError("Given object cannot be an empty Collection.")
+        if len(target) == 0:
+            raise AssertionError("Given target cannot be an empty Collection.")
+
+        # Assert all Timeseries have the same domain
+        objects_domain = object[0].domain
+        if any([x.domain != objects_domain for x in object]) or any([x.domain != objects_domain for x in target]):
+            raise AssertionError("All Timeseries must have the same domain in a SegmentToSegmentDataset.")
+
+        # Assert all Object Timeseries have the same sampling frequency
+        objects_sampling_frequency = object[0].sampling_frequency
+        if any([x.sampling_frequency != objects_sampling_frequency for x in object]):
+            raise AssertionError("All object Timeseries must have the same sampling frequency in a SegmentToSegmentDataset.")
+
+        # Assert all Target Timeseries have the same sampling frequency
+        targets_sampling_frequency = target[0].sampling_frequency
+        if any([x.sampling_frequency != targets_sampling_frequency for x in target]):
+            raise AssertionError("All target Timeseries must have the same sampling frequency in a SegmentToSegmentDataset.")
+
+        # Gets samples from each Segment of each Timeseries.
+        object_all_segments = np.array([timeseries._to_array() for timeseries in object])
+        target_all_segments = np.array([timeseries._to_array() for timeseries in target])
+
+        # VStacks the segments of all Timeseries. Each item is a sample to be fed to the model.
+        self._BiosignalDataset__objects = object_all_segments.swapaxes(0, 1)
+        self._BiosignalDataset__targets = target_all_segments.swapaxes(0, 1)
+
