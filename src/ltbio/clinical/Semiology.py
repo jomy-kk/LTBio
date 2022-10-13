fe94c5a5a7c8616ace2f9f8b2a6e59b176967c07
João Saraiva <joaomiguelsaraiva@tecnico.ulisboa.pt>
2022-10-13 08:51:18 +0100

    Add Semiology to .clinical package
    Includes typical signs and symptoms of epileptic seizures

---
+++ src/ltbio/clinical/Semiology.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+
+# ===================================
+
+# IT - LongTermBiosignals
+
+# Package: clinical
+# Module: Semiology
+# Description: Enumeration Semiology, with multiple common signs, symptoms and other clinical manifestations.
+
+# Contributors: João Saraiva, Mariana Abreu
+# Created: 27/09/2022
+
+# ===================================
+
+from enum import unique, Enum
+
+@unique
+class Semiology(str, Enum):
+
+    MOTOR = "Non-specific Motor"
+    NONMOTOR = "Non-specific Non-motor"
+
+    # Usually epilepsi-related
+    TONIC = "Tonic"
+    ATONIC = "Atonic"
+    CLONIC = "Clonic"
+    TC = "Tonic-Clonic"
+    MYOCLONIC = "Myoclonic"
+    ESPASMS = "Epileptic Spasms"
+    HK = "Hyperkinetic"
+    AUTOMATISMS = "Automatisms"
+    AUTONOMIC = "Autonomic"
+    BARREST = "Behaviour arrest"
+    COGNITIVE = "Cognitive"
+    EMOTIONAL = "Emotional"
+    SENSORY = "Sensory"
+    ABSENCE = "Absence"
+
+    AURA = "Pre-ical Aura"
+
+    SUBCLINICAL = "Sub-clinical / Infra-clinical"
+
+
+
--- src/ltbio/clinical/__init__.py
+++ src/ltbio/clinical/__init__.py
@@ -1,3 +1,4 @@
 # Quick shortcuts to classes
 from ltbio.clinical.BodyLocation import BodyLocation
+from ltbio.clinical.Semiology import Semiology
 from ltbio.clinical.Patient import Patient
