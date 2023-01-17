e0f540e43cc1a1e2b1017325beee4c50f46a3bbb
João Saraiva <joaomiguelsaraiva@tecnico.ulisboa.pt>
2023-01-17 16:37:26 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,8 @@ author = 'João Saraiva'
 # The full version, including alpha/beta/rc tags
 release = '1.0.1'
 
+master_doc = 'index'
+
 
 # -- General configuration ---------------------------------------------------
 
