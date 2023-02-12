0320e2ae8800007af4a3171deb9c0885a097662e
João Saraiva <joaomiguelsaraiva@tecnico.ulisboa.pt>
2023-02-12 13:57:23 +0000

    Add some quality indexes for TEMP

--- src/ltbio/biosignals/modalities/TEMP.py
+++ src/ltbio/biosignals/modalities/TEMP.py
@@ -13,6 +13,10 @@
 # Last Updated: 09/07/2022
 
 # ===================================
+from datetime import timedelta
+
+from datetimerange import DateTimeRange
+from numpy import mean
 
 from ltbio.biosignals.modalities.Biosignal import Biosignal
 from ltbio.biosignals.timeseries.Unit import DegreeCelsius, Multiplier
@@ -27,3 +31,31 @@ class TEMP(Biosignal):
 
     def plot_summary(self, show: bool = True, save_to: str = None):
         pass
+
+    @property
+    def preview(self):
+        """Returns 2 minutes of the middle of the signal."""
+        domain = self.domain
+        middle_of_domain: DateTimeRange = domain[len(domain) // 2]
+        middle = middle_of_domain.start_datetime + (middle_of_domain.timedelta / 2)
+        try:
+            return self[middle - timedelta(seconds=2): middle + timedelta(minutes=2)]
+        except IndexError:
+            raise AssertionError(
+                f"The middle segment of {self.name} from {self.patient_code} does not have at least 5 seconds to return a preview.")
+
+    @staticmethod
+    def racSQI(samples):
+        """
+        Rate of Amplitude change (RAC)
+        It is recomended to be analysed in windows of 2 seconds.
+        """
+        max_, min_ = max(samples), min(samples)
+        amplitude = max_ - min_
+        return abs(amplitude / max_)
+
+    def acceptable_quality(self):  # -> Timeline
+        """
+        Suggested by Böttcher et al. Scientific Reports, 2022, for wearable wrist TEMP.
+        """
+        return self.when(lambda x: 25 < mean(x) < 40 and TEMP.racSQI(x) < 0.2, window=timedelta(seconds=2))
