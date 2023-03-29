a6e4be78f91430c888239cede356282eb8085208
Rafael Silva <83774433+rafaelscsilva@users.noreply.github.com>
2023-03-29 15:33:10 +0100

    Add timeseries method
    Includes aux_date method to extract starting time

--- src/ltbio/biosignals/sources/AFDB.py
+++ src/ltbio/biosignals/sources/AFDB.py
@@ -18,7 +18,10 @@
 import os
 
 import wfdb
+from dateutil.parser import parse as to_datetime
 
+from .. import timeseries
+from .. import modalities
 from ..sources.BiosignalSource import BiosignalSource
 from ltbio.biosignals.timeseries.Unit import *
 from ...clinical import Patient, BodyLocation
@@ -54,6 +57,21 @@ class AFDB(BiosignalSource):
         return record, header, annotation
 
     @staticmethod
+    def __aux_date(header):
+        """
+        Get starting time from header
+        param: header (wfdb.Header)
+        """
+
+        # get hour from header
+        time = str(header.base_time) if header.base_time is not None else '00:00:00'
+
+        # get date from header
+        date = str(header.base_date) if header.base_date is not None else '2000-01-01'
+
+        return to_datetime(date + ' ' + time)
+
+    @staticmethod
     def _name(path, type, **options):
         """
         Defines the name of the biosignal file.
@@ -81,7 +99,41 @@ class AFDB(BiosignalSource):
 
     @staticmethod
     def _timeseries(path: str, type, **options):
-        pass
+        """
+        Defines the timeseries of the biosignal file.
+        param: path (str) path to the file
+        param: type (modalities) modality of the biosignal
+        return: data (dict) dictionary with the timeseries of each channel
+        """
+
+        # check if modality is ECG
+        if type != modalities.ECG:
+            raise Exception("This source only supports ECG modality.")
+
+        # read data
+        record, header, _ = AFDB.__read_data(path)
+
+        data = dict()
+
+        # one key for each channel
+        for ind, channel in enumerate(record.sig_name):
+
+            # set data
+            signal = record.p_signal[:, ind]
+            unit = Volt(Multiplier.m) if 'mV' in header.units[ind] else None
+            fs = header.fs if header.fs is not None else 250
+            start_time = AFDB.__aux_date(header)
+
+            # create timeseries
+            data[channel] = timeseries.Timeseries(
+                samples=signal,
+                units=unit,
+                sampling_frequency=fs,
+                initial_datetime=start_time,
+                name=channel
+            )
+
+        return data
 
     @staticmethod
     def _write(path: str, timeseries: dict):
