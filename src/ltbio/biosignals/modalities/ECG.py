02cbf70cc35917433cdf15d59aa9429648c1d233
João Saraiva <joaomiguelsaraiva@tecnico.ulisboa.pt>
2023-05-03 10:24:49 +0200

    Add ECG.acceptable_quality based on BioSPPy

--- src/ltbio/biosignals/modalities/ECG.py
+++ src/ltbio/biosignals/modalities/ECG.py
@@ -505,6 +505,19 @@ class ECG(Biosignal):
                 res[channel_name] = average(array(res[channel_name]),
                                             weights=list(map(lambda subdomain: subdomain.timedelta.total_seconds(), channel.domain)))
 
+    def acceptable_quality(self):
+        """
+        Suggested for single-lead ECG in
+        Zhao, Z., & Zhang, Y. (2018), SQI quality evaluation mechanism of single-lead ECG signal based on simple
+        heuristic fusion and fuzzy comprehensive evaluation, Frontiers in Physiology, 9, 727.
+        """
+        def aux(x, fs):
+            peaks1 = self.__biosppy_r_indices(x, fs, hamilton_segmenter)
+            peaks2 = self.__biosppy_r_indices(x, fs, christov_segmenter)
+            return ZZ2018(x, peaks1, peaks2, fs=fs, mode='fuzzy')
+
+        return self.when(lambda x: aux(x, self.sampling_frequency) == 'Excellent', window=timedelta(seconds=10))
+
 
 class RRI(DerivedBiosignal):
 
