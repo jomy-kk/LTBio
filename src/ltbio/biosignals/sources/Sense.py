708b9a2258e7865e7d9f8e178a1d9c3e2362d940
João Saraiva <joaomiguelsaraiva@tecnico.ulisboa.pt>
2023-05-05 18:21:16 +0200

    Fix flatline and discrepency thresholds on Sense.onbody for ECG

--- src/ltbio/biosignals/sources/Sense.py
+++ src/ltbio/biosignals/sources/Sense.py
@@ -25,6 +25,7 @@ from warnings import warn
 
 import numpy as np
 from dateutil.parser import parse as to_datetime
+from scipy.stats import stats
 
 from .. import timeseries
 from .. import modalities
@@ -411,33 +412,104 @@ class Sense(BiosignalSource):
                 if unit == Volt(Multiplier.m):
                     FLATLINE_THRESHOLD = 0.1
                 elif unit is None:  # raw
-                    FLATLINE_THRESHOLD = 30
-                    DISCREPANCY_COEFFICIENT = 0.8
+                    # For derivatives
+                    FLATLINE_THRESHOLD = 400
+                    BASELINE_FLATLINE_THRESHOLD = 150
+                    DISCREPEANCY_THRESHOLD = 2000
+                    # For amplitudes
+                    LOW_SATURATION_THRESHOLD = 500
+                    HIGH_SATURATION_THRESHOLD = 3500
+
                 else:
                     raise ValueError(
                             f"All channels are in {unit}. Please convert them all to mV or raw.")
 
-                def flatline(x):
-                    return np.all(np.abs(np.diff(x)) < FLATLINE_THRESHOLD)
+                def total_flatline(x, threshold=FLATLINE_THRESHOLD):
+                    """All the segment must be flat."""
+                    derivative = np.abs(np.diff(x))
+                    return np.all(derivative < threshold)
+
+                def partial_flatline(x):
+                    """At least 1/3 of the segment must be flat."""
+                    derivative = np.abs(np.diff(x))
+                    return np.count_nonzero(derivative < FLATLINE_THRESHOLD) > 1/3 * len(derivative)
+
+                def discrepancy(x):
+                    """
+                    When there is at least one derivative point that is above the discrepancy threshold.
+                    """
+                    derivative = np.abs(np.diff(x))
+                    return np.any(derivative > DISCREPEANCY_THRESHOLD)
 
                 def low_saturation_region(x):
-                    # Lowest saturation region is usually attained when - electrode loses contact
-                    derivative = np.abs(np.diff(x))
-                    return np.any(x < 20) and flatline(x)  # Amplitude < 20 and flatline
+                    """
+                    All the segment must be below low saturation threshold.
+                    Lowest saturation region is usually attained when - electrode loses contact.
+                    """
+                    return np.all(x < LOW_SATURATION_THRESHOLD) and total_flatline(x)  # Amplitude < 15 and flatline
 
                 def high_saturation_region(x):
-                    # Highest saturation region is usually attained when + electrode loses contact
-                    derivative = np.abs(np.diff(x))
-                    return np.any(x > 3900) and flatline(x)  # Amplitude > 3900 and flatline
+                    """
+                    All the segment must be above high saturation threshold.
+                    Highest saturation region is usually attained when + electrode loses contact.
+                    """
+                    return np.all(x > HIGH_SATURATION_THRESHOLD) and total_flatline(x)  # Amplitude > 3600 and flatline
+
+                def low_high_saturation_transition(x):
+                    """
+                    Transition from low to high saturation region.
+                    There must exist samples in the low and high saturation regions, and partial flatline in between.
+                    """
+                    return np.max(x) - np.min(x) > HIGH_SATURATION_THRESHOLD-LOW_SATURATION_THRESHOLD and discrepancy(x)
+
+                def electronic_noise(x):
+                    """
+                    Electronic noise is usually attained when the reference electrode is in place, but the + and - electrodes are not.
+                    There is no flatline, but the signal is very noisy.
+                    It can be modeled as gaussian noise ~ N(0, max-min).
+                    """
+                    return False
+
+                def baseline_saturation(x):
+                    """
+                    Baseline saturation is usually attained when both + and + electrodes plus reference/ground lose contact.
+                    There is total flatline and the signal is saturated at the baseline for periods longer than usual heartbeats,
+                    so this should be applied only to segments of a typical heartbeat length.
+                    Since we are checking in a longer period, the derivative threshold is lower, to be more strict.
+                    -----
+                    E.g.: Using a window of 800 ms, the derivative threshold should be lower than 50 during all 800ms.
+                    This would not be the case in a shorter window.
+                    """
+                    decision = total_flatline(x, BASELINE_FLATLINE_THRESHOLD)
+                    if False:
+                        print("DECISION 2: Baseline Saturated?", decision)
+                        print("derivatives:", np.abs(np.diff(x)))
+                        print("------")
+                    return decision
 
                 def saturated(x):
-                    return low_saturation_region(x) or high_saturation_region(x)
+                    a = low_saturation_region(x)
+                    b = high_saturation_region(x)
+                    c = low_high_saturation_transition(x)
+                    decision = a or b or c
+                    if False:
+                        print("Mean:", np.mean(x), "Median:", np.median(x), "Max:", np.max(x), "Min:", np.min(x))
+                        print("Any Below 20:", np.any(x < LOW_SATURATION_THRESHOLD))
+                        print("Any Above 3900:", np.any(x > HIGH_SATURATION_THRESHOLD))
+                        print("Derivative:", np.abs(np.diff(x-x.mean())), "Total flatline" if total_flatline(x) else "Partial flatline" if partial_flatline(x) else "No flatline detected")
+                        print("Discrepancy:", discrepancy(x))
+                        print("DECISION: Saturated?", decision)
+                        print("\t\tBecause of:", f"low_saturation_region:{a} |", f"high_saturation_region:{b} |", f"low_high_saturation_transition:{c}")
+                        print("-----")
+                    return decision
 
-                not_saturated = biosignal.when(lambda x: not saturated(x), timedelta(milliseconds=50))
-                not_flat = biosignal.when(lambda x: not flatline(x), timedelta(milliseconds=500))
-                electronic_noise = biosignal.when(lambda x: 0.05 < np.var(x)/np.mean(x) < 150., timedelta(milliseconds=500))
+                # Resample to 200 Hz, because the saturation thresholds are defined for 200 Hz.
+                resampled_biosignal = biosignal.__copy__()
+                resampled_biosignal.resample(200)
+                not_saturated = resampled_biosignal.when(lambda x: not saturated(x), timedelta(milliseconds=50))
+                not_baseline_saturated = resampled_biosignal.when(lambda x: not baseline_saturation(x), timedelta(milliseconds=800))
 
-                onbody = Timeline.intersection(not_saturated, not_flat, electronic_noise)
+                onbody = Timeline.intersection(not_saturated, not_baseline_saturated)
 
         # Not yet implemented for other modalities or locations
 
