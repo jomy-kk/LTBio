4cb787d36a32d358ce8ee58f457c7381e7596398
saraiva <joaomiguelsaraiva@tecnico.ulisboa.pt>
2023-05-16 14:06:02 +0200

    Make G unit's SHORT lowercase

--- src/ltbio/biosignals/timeseries/Unit.py
+++ src/ltbio/biosignals/timeseries/Unit.py
@@ -104,7 +104,7 @@ class G(Unit):
     def __init__(self, multiplier=Multiplier._):
         super().__init__(multiplier)
 
-    SHORT = "G"
+    SHORT = "g"
 
     def convert_to(self, unit):
        pass
