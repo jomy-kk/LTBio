41164afce4b57c79601f7beb07c47d7af44c40f3
saraiva <joaomiguelsaraiva@tecnico.ulisboa.pt>
2023-10-23 11:13:57 +0200

    Fix Segment test set properties

--- tests/biosignals/Segment/test_set_properties.py
+++ tests/biosignals/Segment/test_set_properties.py
@@ -1,5 +1,7 @@
 import unittest
 
+from numpy import allclose
+
 from resources.segments import get_segment, small_samples_1, small_samples_2
 
 
@@ -10,11 +12,10 @@ class SegmentSetPropertiesTestCase(unittest.TestCase):
         cls.small_segment = get_segment('small', 1)
 
     def test_set_samples(self):
-        samples = self.small_segment.samples
         # Try to set samples
         with self.assertRaises(AttributeError):
             self.small_segment.samples = small_samples_2
-        self.assertEqual(samples, small_samples_1)
+        self.assertTrue(allclose(self.small_segment.samples, small_samples_1))
 
 
 if __name__ == '__main__':
