71b423254017782e52ebb487d2bb926eec753ce9
saraiva <joaomiguelsaraiva@tecnico.ulisboa.pt>
2023-10-23 11:15:57 +0200

    Add 1 Segment initializer tests

--- tests/biosignals/Segment/test_initializers.py
+++ tests/biosignals/Segment/test_initializers.py
@@ -2,6 +2,7 @@ import unittest
 
 from numpy import ndarray
 
+from ltbio._core.exceptions import EmptySegmentError
 from ltbio.biosignals import Segment
 from tests.resources.segments import large_samples_3
 
@@ -18,6 +19,10 @@ class SegmentInitializersTestCase(unittest.TestCase):
         self.assertTrue(all(samples == large_samples_3))
         # but not pointer
         self.assertFalse(samples is large_samples_3)
+
+    def test_initialize_with_empty_samples_raises_error(self):
+        with self.assertRaises(EmptySegmentError):
+            Segment([])
 
         
 if __name__ == '__main__':
