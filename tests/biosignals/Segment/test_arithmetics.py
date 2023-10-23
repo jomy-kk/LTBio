fe6ac45fd64644ead86fe3d07a69e8b6ace53489
saraiva <joaomiguelsaraiva@tecnico.ulisboa.pt>
2023-10-23 11:16:11 +0200

    Fix Segment arithmetic tests

--- tests/biosignals/Segment/test_arithmetics.py
+++ tests/biosignals/Segment/test_arithmetics.py
@@ -1,5 +1,6 @@
 import unittest
 
+import numpy as np
 from numpy import allclose
 
 from ltbio._core.exceptions import DifferentLengthsError
@@ -11,76 +12,71 @@ class SegmentArithmeticsTestCase(unittest.TestCase):
 
     LENGTH = 'small'
 
-    def _assert_arithmetic_operation(self, operation, a, b, a_content, b_content):
-        expected_sum = operation(a_content, b_content)
+    def _assert_arithmetic_operation(self, operation_outplace, operation_inplace, a, b, a_content, b_content, expected_result):
         # Out of place
-        result = operation(a, b)
-        self.assertIsInstance(result, Segment)
-        self.assertEqual(len(result), get_segment_length(self.LENGTH))
-        self.assertTrue(allclose(result.samples, expected_sum))
+        result = operation_outplace(a, b)
+        self.assertIsInstance(result, Segment)  # check if a Segment
+        self.assertEqual(len(result), get_segment_length(self.LENGTH))  # check if correct length
+        self.assertTrue(allclose(result.samples, expected_result))  # check if correct content
+        self.assertTrue(allclose(a.samples, a_content))  # check if intact
+        self.assertTrue(allclose(b.samples if isinstance(b, Segment) else b, b_content))  # check if intact
+        self.assertTrue(a is not result and b is not result)  # check pointers
+
         # In place
-        a += b
-        self.assertEqual(len(a), get_segment_length(self.LENGTH))
-        self.assertTrue(allclose(a.samples, expected_sum))
+        operation_inplace(a, b)
+        self.assertIsInstance(a, Segment)  # check if still a Segment
+        self.assertEqual(len(a), get_segment_length(self.LENGTH))  # check if correct length
+        self.assertTrue(allclose(a.samples, expected_result))  # check if correct content
+        self.assertTrue(allclose(b.samples if isinstance(b, Segment) else b, b_content))  # check if intact
 
     def test_add_two_segments(self):
-        for operation in (Segment.__add__, Segment.__iadd__):
-            self._assert_arithmetic_operation(operation,
-                                              get_segment(self.LENGTH, 1), get_segment(self.LENGTH, 2),
-                                              small_samples_1, small_samples_2)
+        self._assert_arithmetic_operation(Segment.__add__, Segment.__iadd__,
+                                          get_segment(self.LENGTH, 1), get_segment(self.LENGTH, 2),
+                                          small_samples_1, small_samples_2, np.add(small_samples_1, small_samples_2))
 
     def test_add_segment_and_number(self):
-        for operation in (Segment.__add__, Segment.__iadd__):
-            self._assert_arithmetic_operation(operation,
-                                          get_segment(self.LENGTH, 1), 30,
-                                          small_samples_1, 30)
+        self._assert_arithmetic_operation(Segment.__add__, Segment.__iadd__,
+                                      get_segment(self.LENGTH, 1), 30,
+                                      small_samples_1, 30, np.add(small_samples_1, 30))
 
     def test_sub_two_segments(self):
-        for operation in (Segment.__sub__, Segment.__isub__):
-            self._assert_arithmetic_operation(operation,
-                                              get_segment(self.LENGTH, 1), get_segment(self.LENGTH, 2),
-                                              small_samples_1, small_samples_2)
+        self._assert_arithmetic_operation(Segment.__sub__, Segment.__isub__,
+                                          get_segment(self.LENGTH, 1), get_segment(self.LENGTH, 2),
+                                          small_samples_1, small_samples_2, np.subtract(small_samples_1, small_samples_2))
     def test_sub_segment_and_number(self):
-        for operation in (Segment.__sub__, Segment.__isub__):
-            self._assert_arithmetic_operation(operation,
-                                          get_segment(self.LENGTH, 1), 30,
-                                          small_samples_1, 30)
+        self._assert_arithmetic_operation(Segment.__sub__, Segment.__isub__,
+                                      get_segment(self.LENGTH, 1), 30,
+                                      small_samples_1, 30, np.subtract(small_samples_1, 30))
 
     def test_mul_two_segments(self):
-        for operation in (Segment.__mul__, Segment.__imul__):
-            self._assert_arithmetic_operation(operation,
-                                              get_segment(self.LENGTH, 1), get_segment(self.LENGTH, 2),
-                                              small_samples_1, small_samples_2)
+        self._assert_arithmetic_operation(Segment.__mul__, Segment.__imul__,
+                                          get_segment(self.LENGTH, 1), get_segment(self.LENGTH, 2),
+                                          small_samples_1, small_samples_2, np.multiply(small_samples_1, small_samples_2))
 
     def test_mul_segment_and_number(self):
-        for operation in (Segment.__mul__, Segment.__imul__):
-            self._assert_arithmetic_operation(operation,
-                                          get_segment(self.LENGTH, 1), 30,
-                                          small_samples_1, 30)
+        self._assert_arithmetic_operation(Segment.__mul__, Segment.__imul__,
+                                      get_segment(self.LENGTH, 1), 30,
+                                      small_samples_1, 30, np.multiply(small_samples_1, 30))
 
     def test_truediv_two_segments(self):
-        for operation in (Segment.__truediv__, Segment.__itruediv__):
-            self._assert_arithmetic_operation(operation,
-                                              get_segment(self.LENGTH, 1), get_segment(self.LENGTH, 2),
-                                              small_samples_1, small_samples_2)
+        self._assert_arithmetic_operation(Segment.__truediv__, Segment.__itruediv__,
+                                          get_segment(self.LENGTH, 1), get_segment(self.LENGTH, 2),
+                                          small_samples_1, small_samples_2, np.true_divide(small_samples_1, small_samples_2))
 
     def test_truediv_segment_and_number(self):
-        for operation in (Segment.__truediv__, Segment.__itruediv__):
-            self._assert_arithmetic_operation(operation,
-                                          get_segment(self.LENGTH, 1), 30,
-                                          small_samples_1, 30)
+        self._assert_arithmetic_operation(Segment.__truediv__, Segment.__itruediv__,
+                                      get_segment(self.LENGTH, 1), 30,
+                                      small_samples_1, 30, np.true_divide(small_samples_1, 30))
 
     def test_floordiv_two_segments(self):
-        for operation in (Segment.__floordiv__, Segment.__ifloordiv__):
-            self._assert_arithmetic_operation(operation,
-                                              get_segment(self.LENGTH, 1), get_segment(self.LENGTH, 2),
-                                              small_samples_1, small_samples_2)
+        self._assert_arithmetic_operation(Segment.__floordiv__, Segment.__ifloordiv__,
+                                          get_segment(self.LENGTH, 1), get_segment(self.LENGTH, 2),
+                                          small_samples_1, small_samples_2, np.floor_divide(small_samples_1, small_samples_2))
 
     def test_floordiv_segment_and_number(self):
-        for operation in (Segment.__floordiv__, Segment.__ifloordiv__):
-            self._assert_arithmetic_operation(operation,
-                                          get_segment(self.LENGTH, 1), 30,
-                                          small_samples_1, 30)
+        self._assert_arithmetic_operation(Segment.__floordiv__, Segment.__ifloordiv__,
+                                      get_segment(self.LENGTH, 1), 30,
+                                      small_samples_1, 30, np.floor_divide(small_samples_1, 30))
 
     def test_arithmetics_with_invalid_types(self):
         for operation in (Segment.__add__, Segment.__iadd__, Segment.__mul__, Segment.__imul__,
