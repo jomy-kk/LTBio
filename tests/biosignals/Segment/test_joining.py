1a3c8587789875832def76d557347e2a9bc3832e
saraiva <joaomiguelsaraiva@tecnico.ulisboa.pt>
2023-10-23 11:15:25 +0200

    Add Segment joining tests

--- tests/biosignals/Segment/test_joining.py
+++ tests/biosignals/Segment/test_joining.py
@@ -1,37 +1,84 @@
 import unittest
 
+import numpy as np
 from numpy import allclose
 
 from ltbio._core.exceptions import DifferentLengthsError
 from ltbio.biosignals import Segment
-from resources.segments import get_segment, get_segment_length, small_samples_1, small_samples_2
+from resources.segments import get_segment, get_segment_length, small_samples_1, medium_samples_1, large_samples_1
 
 
 class SegmentJoiningTestCase(unittest.TestCase):
-    def _assert_arithmetic_operation(self, operation, a, b, a_content, b_content):
-        expected_sum = operation(a_content, b_content)
-        # Out of place
-        result = operation(a, b)
-        self.assertIsInstance(result, Segment)
-        self.assertEqual(len(result), get_segment_length(self.LENGTH))
-        self.assertTrue(allclose(result.samples, expected_sum))
-        # In place
-        a += b
-        self.assertEqual(len(a), get_segment_length(self.LENGTH))
-        self.assertTrue(allclose(a.samples, expected_sum))
 
-    def test_concatenate_one_segment(self):
-        pass
+    def setUp(cls):
+        cls.group = 1
+        cls.small_segment = get_segment('small', cls.group)
+        cls.medium_segment = get_segment('medium', cls.group)
+        cls.large_segment = get_segment('large', cls.group)
 
-    def test_concatenate_multiple_segments(self):
-        pass
+    def test_append_more_samples(self):
+        # Assert before
+        self.assertEqual(len(self.large_segment), get_segment_length('large'))  # Length
+        self.assertTrue(allclose(self.large_segment.samples, large_samples_1))  # Content
 
-    def test_append_array(self):
-        pass
+        # Append medium samples
+        self.large_segment.append(medium_samples_1)
+        expected_content = np.concatenate((large_samples_1, medium_samples_1))
 
-    def test_append_sequence(self):
-        pass
+        # Assert after
+        self.assertEqual(len(self.large_segment), get_segment_length('large') + get_segment_length('medium'))  # Length affected
+        self.assertEqual(len(medium_samples_1), get_segment_length('medium'))  # Length not affected
+        self.assertTrue(allclose(self.large_segment.samples, expected_content))  # Content
 
-        
+        # Append a list
+        to_append = [1, 2, 3]
+        self.large_segment.append(to_append)
+        expected_content = np.concatenate((expected_content, to_append))
+
+        # Assert after
+        self.assertEqual(len(self.large_segment), get_segment_length('large') + get_segment_length('medium') + len(to_append))  # Length affected
+        self.assertEqual(len(to_append), 3)  # Length not affected
+        self.assertEqual(len(medium_samples_1), get_segment_length('medium'))  # Length not affected
+        self.assertTrue(allclose(self.large_segment.samples, expected_content))  # Content
+
+    def test_append_type_error(self):
+        for invalid_type in (1, 1.0, True, False, None, {1, 2, 3}, {'a': 1, 'b': 2}, 'string'):
+            with self.assertRaises(TypeError):
+                self.small_segment.append(invalid_type)
+
+    def test_concatenate_two_segments(self):
+        # Assert before
+        self.assertTrue(allclose(self.large_segment.samples, large_samples_1))
+        self.assertTrue(allclose(self.medium_segment.samples, medium_samples_1))
+
+        res = Segment.concatenate(self.large_segment, self.medium_segment)
+        expected_content = np.concatenate((large_samples_1, medium_samples_1))
+
+        # Assert after
+        self.assertTrue(allclose(self.large_segment.samples, large_samples_1))
+        self.assertTrue(allclose(self.medium_segment.samples, medium_samples_1))
+        self.assertTrue(allclose(res.samples, expected_content))
+
+    def test_concatenate_three_segments(self):
+        # Assert before
+        self.assertTrue(allclose(self.large_segment.samples, large_samples_1))
+        self.assertTrue(allclose(self.medium_segment.samples, medium_samples_1))
+        self.assertTrue(allclose(self.small_segment.samples, small_samples_1))
+
+        res = Segment.concatenate(self.large_segment, self.medium_segment, self.small_segment)
+        expected_content = np.concatenate((large_samples_1, medium_samples_1, small_samples_1))
+
+        # Assert after
+        self.assertTrue(allclose(self.large_segment.samples, large_samples_1))
+        self.assertTrue(allclose(self.medium_segment.samples, medium_samples_1))
+        self.assertTrue(allclose(self.small_segment.samples, small_samples_1))
+        self.assertTrue(allclose(res.samples, expected_content))
+
+    def test_concatenate_type_error(self):
+        for invalid_type in (1, 1.0, True, False, None, {1, 2, 3}, {'a': 1, 'b': 2}, 'string'):
+            with self.assertRaises(TypeError):
+                Segment.concatenate(self.small_segment, invalid_type)
+
+
 if __name__ == '__main__':
     unittest.main()
