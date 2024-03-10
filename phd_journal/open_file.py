f4d1ad5f33d44d2b8f7d12df43d6c7ad40c06978
saraiva <joaomiguelsaraiva@tecnico.ulisboa.pt>
2024-03-10 18:55:35 +0100

    Delete research folders

--- phd_journal/open_file.py
+++
@@ -1,6 +0,0 @@
-from ltbio.biosignals.modalities import EEG
-from phd_journal.DZNE import DZNE
-
-common_path = '/Users/saraiva/Desktop/Doktorand/Data/DZNE/eeg_data_teipel'
-x = EEG(common_path, DZNE, age_group='05_10', subject_id=1)
-
